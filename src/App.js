d25a0c88cfa9161ddd16654ee06e4943660978c5
tieuminh7 <tieuminh.flancerwork@gmail.com>
2024-09-14 14:48:42 +0700

    p2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">minh</div>;
+  const [content, setContent] = useState("");
+
+  return (
+    <div className="flex gap-8 justify-center items-center h-screen">
+      <input
+        type="text"
+        className="outline-none border border-green-600 px-4 py-2 w-[400px]"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        ADD
+      </button>
+    </div>
+  );
 }
 
 export default App;
